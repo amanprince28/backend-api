faa858d11fa9d52d78d6fcf8ffe5a203440e8db4
Aman <amanprince28@gmail.com>
2025-03-28 21:13:04 +0800

    disable auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,10 +15,11 @@ constructor(private authService: AuthService) {}
     return this.authService.login(req.user);
   }
 
-  @UseGuards(AuthGuard)
+  // @UseGuards(AuthGuard)
   @Put('change-password')
   async changePassword(@Headers() headers, @Body() changePasswordDto: ChangePasswordDto) {
-    await this.authService.changePassword(headers.auth_user.sub, changePasswordDto);
+    console.log(headers.user_id)
+    await this.authService.changePassword(headers.user_id, changePasswordDto);
     return { message: 'Password changed successfully' };
   }
 }
